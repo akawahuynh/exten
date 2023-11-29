ec5c217c88e76491fdede2450c44c2aa7889484b
akawahuynh <akawahuynh3006@gmail.com>
2023-11-29 14:51:04 +0700

    add

---
+++ script/script.js
@@ -0,0 +1,11 @@
+async function fetchData() {
+    //api next version
+    // const res=await fetch ("");
+    // const record=await res.json();
+    // document.getElementById("date").innerHTML=record.data[0].date;
+    // document.getElementById("areaName").innerHTML=record.data[0].areaName;
+    // document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+    // document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+    // console.log(res)
+}
+
